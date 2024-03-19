8f26fb9d88c0654cdd60beb52f3eb2099fecdfea
Rajský Matyáš <149485684+rrrajsky@users.noreply.github.com>
2024-03-19 13:05:22 +0100

    Update Ucet.cs

--- Ucet.cs
+++ Ucet.cs
@@ -12,16 +12,20 @@ namespace ConsoleApp1
         private int id;
         private string username;
         private string password;
+        private TypUctu typUctu;
+
 
         public int ID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public TypUctu TypUctu { get; set; }
 
-        public Ucet(int id, string username, string password)
+        public Ucet(int id, string username, string password, TypUctu typUctu)
         {
             this.id = id;
             this.username = username;
             this.password = password;
+            this.typUctu = typUctu;
         }
 
         public Ucet()
