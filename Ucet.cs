9a230d775a47b3d6a97622e6e1d83978df3e41b0
Rajský Matyáš <149485684+rrrajsky@users.noreply.github.com>
2024-03-19 13:08:48 +0100

    Update Ucet.cs

--- Ucet.cs
+++ Ucet.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,19 +13,22 @@ namespace ConsoleApp1
         private int id;
         private string username;
         private string password;
+        private int balance;
         private TypUctu typUctu;
 
 
         public int ID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public int Balance { get; set; }
         public TypUctu TypUctu { get; set; }
 
-        public Ucet(int id, string username, string password, TypUctu typUctu)
+        public Ucet(int id, string username, string password, int balance,TypUctu typUctu)
         {
             this.id = id;
             this.username = username;
             this.password = password;
+            this.balance = balance;
             this.typUctu = typUctu;
         }
 
